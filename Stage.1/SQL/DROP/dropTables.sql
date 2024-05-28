4053b73aadf4ede834d35fcbfc34dde4cb7a15e8
Shay Mordechai <52373104+shay0129@users.noreply.github.com>
2024-05-28 16:59:35 +0300

    Update dropTables.sql

--- Stage.1/SQL/DROP/dropTables.sql
+++ Stage.1/SQL/DROP/dropTables.sql
@@ -1,1 +1,12 @@
+-- Revoke grants before dropping tables
+REVOKE UPDATE (Budget_Code, Sum) ON Budget FROM 'Budget Committee';
+REVOKE UPDATE (Quantity) ON `Order` FROM 'Procurement Manager';
 
+-- Drop tables in the reverse order they were created in your schema 
+-- (to avoid foreign key constraint errors)
+DROP TABLE IF EXISTS Invoice;
+DROP TABLE IF EXISTS Payment;
+DROP TABLE IF EXISTS `Order`;
+DROP TABLE IF EXISTS Supplier;
+DROP TABLE IF EXISTS Employee;
+DROP TABLE IF EXISTS Budget;
