05387a443d211da1d0913edb691b5d0ac0073201
Shay Mordechai <52373104+shay0129@users.noreply.github.com>
2024-05-29 22:29:38 +0300

    Update createTables.sql

--- Stage.1/SQL/CREATE/createTables.sql
+++ Stage.1/SQL/CREATE/createTables.sql
@@ -1,63 +1,49 @@
-CREATE TABLE Budget
-(
-  Budget_Code INT NOT NULL AUTO_INCREMENT,
-  Employee_ID INT NOT NULL,
-  Category VARCHAR(255) NOT NULL,
-  Sum DECIMAL(10,2) NOT NULL,
-  Year INT NOT NULL,
-  PRIMARY KEY (Budget_Code),
-  FOREIGN KEY (Employee_ID) REFERENCES Employee(Employee_ID)
+CREATE TABLE Employee (
+   Employee_ID NUMBER(4) PRIMARY KEY,
+   Employee_Name VARCHAR(30),
+   Seniority NUMBER(5),
+   Contact_Information VARCHAR(50),
+   Job_Title  VARCHAR(30) CHECK (Job_Title IN ('Budget Committee', 'Procurement Manager', 'Worker'))
 );
 
-GRANT UPDATE (Budget_Code, Sum) ON Budget TO 'Budget Committee';
-
-CREATE TABLE Employee 
-(
-  Employee_Name VARCHAR(255) NOT NULL,
-  Employee_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  Seniority INT NOT NULL,
-  Contact_Information ENUM('Email', 'Phone Number') NOT NULL,
-  Role ENUM('Budget Committee', 'Procurement Manager', 'Worker') NOT NULL
+CREATE TABLE Budget (
+   Budget_Code INT PRIMARY KEY,
+   Employee_ID NUMBER(4),
+   Expense_Category VARCHAR(50),
+   Budget_Amount DECIMAL(10,2),
+   Budget_Year NUMBER(4),
+   FOREIGN KEY (Employee_ID) REFERENCES Employee(Employee_ID)
 );
 
-CREATE TABLE `Order`
-(
-  Order_ID INT NOT NULL AUTO_INCREMENT,
-  Employee_Name VARCHAR(255) NOT NULL,
-  Quantity INT NOT NULL,
-  Supplier_ID INT NOT NULL,
-  PRIMARY KEY (Order_ID),
-  FOREIGN KEY (Supplier_ID) REFERENCES Supplier(Supplier_ID)
+CREATE TABLE Supplier (
+   Supplier_ID INT PRIMARY KEY,
+   Supplier_Name VARCHAR(30),
+   Contact_Information VARCHAR(50),
+   Inventory DECIMAL(10,2)
 );
 
-GRANT UPDATE (Quantity) ON `Order` TO 'Procurement Manager';
-
-CREATE TABLE Supplier 
-(
-  Supplier_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  Supplier_Name VARCHAR(255) NOT NULL,
-  Contact_Information ENUM('Email', 'Phone Number') NOT NULL,
-  Inventory DECIMAL(10,2) NOT NULL
+CREATE TABLE Orders (
+   Order_ID INT PRIMARY KEY,
+   Supplier_ID INT,
+   Employee_ID INT,
+   Quantity INT,
+   FOREIGN KEY (Employee_ID) REFERENCES Employee(Employee_ID),
+   FOREIGN KEY (Supplier_ID) REFERENCES Supplier(Supplier_ID)
 );
 
-CREATE TABLE Invoice 
-(
-  Invoice_ID INT NOT NULL AUTO_INCREMENT,
-  Order_ID INT NOT NULL,
-  Cost DECIMAL(10,2) NOT NULL,
-  Date DATE NOT NULL,
-  PRIMARY KEY (Invoice_ID),
-  FOREIGN KEY (Order_ID) REFERENCES `Order`(Order_ID)
+CREATE TABLE Invoice (
+   Invoice_ID INT PRIMARY KEY,
+   Order_ID INT,
+   Invoice_Cost DECIMAL(10,2),
+   Invoice_Date DATE,
+   FOREIGN KEY (Order_ID) REFERENCES Orders(Order_ID)
 );
 
-CREATE TABLE Payment 
-(
-  Payment_ID INT NOT NULL AUTO_INCREMENT,
-  Employee_ID INT NOT NULL,
-  Amount DECIMAL(10,2) NOT NULL,
-  Payment_Purpose ENUM('Salary', 'Bonus', 'Grant') NOT NULL,
-  Date DATE NOT NULL,
-  PRIMARY KEY (Payment_ID),
-  FOREIGN KEY (Employee_ID) REFERENCES Employee(Employee_ID)
+CREATE TABLE Payment (
+   Payment_ID INT PRIMARY KEY,
+   Employee_ID INT,
+   Amount DECIMAL(10,2),
+   Payment_Purpose VARCHAR(15),
+   Payment_Date DATE,
+   FOREIGN KEY (Employee_ID) REFERENCES Employee(Employee_ID)
 );
-
